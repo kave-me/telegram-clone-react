81f7e9d0095bfe24a2d51eba980a44ef45300c24
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-19 19:01:01 +0430

    align vertical BurgerIcon.tsx

--- src/components/BurgerIcon.tsx
+++ src/components/BurgerIcon.tsx
@@ -1,4 +1,4 @@
-const BurgerIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 " viewBox="0 0 20 20" fill="gray">
+const BurgerIcon = () => <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-8 " viewBox="0 0 20 20" fill="gray">
   <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
 </svg>
 
