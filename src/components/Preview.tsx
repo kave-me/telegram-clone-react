3509a90d11b927592f88badce34039e236f1d2bb
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-19 19:06:23 +0430

    change font style of NothingToShow component

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -5,7 +5,7 @@ function ActivePreview() {
 function NothingToPreview() {
   return <div className="w-full h-screen bg-gray-800 flex justify-center items-center">
 
-    <p className="bg-gray-700 rounded-md text-gray-200 text-sm px-3"> Select a chat to start messaging </p>
+    <p className="bg-gray-700 rounded-xl text-gray-100 text-sm py-1 px-3"> Select a chat to start messaging </p>
     </div>;
 }
 
