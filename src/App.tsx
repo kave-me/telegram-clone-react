1481b372697150a3e3b76085ce332ca617c0edce
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-22 15:15:44 +0430

    change layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Preview from "./components/Preview";
+import useAPP from "./store/useApp";
 
 function App() {
+  const setTargetUser = useAPP((store) => store.setTargetUser);
+  useEffect(() => {
+    const ESCAPE = "Escape";
+    document.addEventListener("keydown", (e) =>
+      e.key === ESCAPE ? setTargetUser(-1) : null
+    );
+  }, [setTargetUser]);
   return (
-    <div className="bg-gray-800 flex items-stretch h-screen">
-      <div className="w-full md:w-2/5 2xl:w-1/4">
-        <Sidebar/>
+    <div className="flex items-stretch h-screen text-gray-100 text-sm">
+      <div className="w-full min-w-max sm:w-1/3 2xl:w-1/4">
+        <Sidebar />
       </div>
-      <div className="hidden md:block flex-grow">
-        <Preview/>
+      <div className="hidden sm:block flex-grow sm:w-2/3 2xl:w-3/4  ">
+        <Preview />
       </div>
     </div>
   );
