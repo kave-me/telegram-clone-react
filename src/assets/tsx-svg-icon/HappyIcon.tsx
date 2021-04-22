fbf306d96001f57924086eec79054d69ac824505
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-22 15:06:36 +0430

    add icons

---
+++ src/assets/tsx-svg-icon/HappyIcon.tsx
@@ -0,0 +1,17 @@
+export function HappyIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-8 w-8 m-1 flex-grow-0 text-gray-500"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor">
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+  );
+}
