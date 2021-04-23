ecc1039c9cf56a7de948b8fc4e37dcbb8b5e9bfb
Kave Mohammadi <kave.mdi@gmail.com>
2021-04-24 00:05:23 +0430

    refactor search bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,11 +2,11 @@ import BurgerIcon from "../assets/tsx-svg-icon/BurgerIcon";
 
 export function SearchBar() {
   return (
-    <div className={"flex flex-stretch h-10 w-full mb-2 mt-4"}>
-      <span className="w-1/6 h-full flex align-middle justify-center ">
+    <div className={"flex items-center flex-stretch h-16 w-full"}>
+      <span className="w-1/6 h-full flex align-middle justify-center items-center">
         <BurgerIcon />
       </span>
-      <span className="flex-grow-1 w-5/6 pr-8">
+      <span className="flex-grow-1 items-center h-10 w-5/6 pr-8">
         <input
           className="w-full h-full px-3 rounded-md bg-gray-600 focus:outline-none focus:ring ring-gray-600"
           placeholder={"Search"}
